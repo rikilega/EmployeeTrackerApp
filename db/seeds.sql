a1aa346b1e764edfa22f3a7245a5896d9fde7c0f
rikilega <rosedoland33@yahoo.com>
2023-05-25 19:04:36 -0700

    modified seeds.sql, added more data for initial table

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,15 +7,17 @@ VALUES
   ('Sales'),
   ('Engineering'),
   ('Finance'),
-  ('Marketing');
+  ('Operations');
 
 -- Insert records into role table
 INSERT INTO role (title, salary, department_id) 
 VALUES 
-  ('Sales Manager', 70000, 1),
-  ('Engineer', 60000, 2),
-  ('Accountant', 50000, 3),
-  ('Marketing Manager', 65000, 4);
+  ('outbound', 50000, 4),
+  ('inbound', 50000, 4),
+  ('ops manager', 70000, 4),
+  ('senior manager', 120000, 4),
+  ('accountant', 65000, 3),
+  ('sales rep', 50000, 1);
 
 -- Select the id of the manager (assume that John Doe is the manager)
 SET @manager_id = (SELECT id FROM employee WHERE last_name = 'Doe' LIMIT 1);
@@ -23,6 +25,9 @@ SET @manager_id = (SELECT id FROM employee WHERE last_name = 'Doe' LIMIT 1);
 -- Insert records into employee table
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES 
-  ('Jane', 'Doe', 2, @manager_id),
-  ('Joe', 'Bloggs', 3, @manager_id),
-  ('Jill', 'Bloggs', 4, @manager_id);
+  ('ron', 'jacobs', 2, NULL),
+  ('rona', 'jan', 1, NULL),
+  ('rik', 'joe', 4, 3),
+  ('Jane', 'Doe', 6, 3),
+  ('doe', 'moe', 3, 4),
+  ('randy', 'ron', 3, 2);
